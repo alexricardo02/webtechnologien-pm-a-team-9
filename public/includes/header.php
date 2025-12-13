0531b9c64961032ce2978b9777836fab6c9a3386
alexricardo02 <brinckmannalex@gmail.com>
2025-12-13 15:03:47 +0100

    feat: more styling changes

--- public/includes/header.php
+++ public/includes/header.php
@@ -6,7 +6,6 @@
 
         <nav class="headerMenu" role="navigation">
             <ul class="navbarList">
-                <li class="navbarItem"><a href="uberblick.php" class="navbarLink">Überblick</a></li>
                 <li class="navbarItem"><a href="raumdimension.php" class="navbarLink">Raumdimension</a></li>
                 <li class="navbarItem"><a href="zeitdimension.php" class="navbarLink">Zeitdimension</a></li>
                 <li class="navbarItem"><a href="datenverwaltung.php" class="navbarLink navbarLink--cta">Datenverwaltung</a></li>

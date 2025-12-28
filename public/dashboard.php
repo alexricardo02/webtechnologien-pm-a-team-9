c8c04ea9731cef318c17e411b8f723d6a7cae6d8
alexricardo02 <brinckmannalex@gmail.com>
2025-12-28 11:57:28 +0100

    more file name changes

--- public/dashboard.php
+++ public/dashboard.php
@@ -24,7 +24,7 @@ $straftat_hauptkategorien = [
   <script src="https://cdnjs.cloudflare.com/ajax/libs/chroma-js/2.4.2/chroma.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.1/dist/chart.umd.min.js"></script>
 
-  <script src="assets/js/raumdimension_chart.js" defer></script>
+  <script src="assets/js/dashboard_chart.js" defer></script>
   <script src="assets/js/gender_chart.js" defer></script>
   <script src="assets/js/crime_comparison_chart.js" defer></script>
   <link rel="stylesheet" href="assets/css/style.css">
